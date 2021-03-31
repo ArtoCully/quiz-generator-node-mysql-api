9c1ccb3dd0917ee6b956824739abdd859fb3c744
Arturo Cullinane <artocully@users.noreply.github.com>
2021-03-31 15:48:50 +0100

    Fix unused error handler

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 // app.use('/users', require('./users/users.controller'));
 
 // global error handler
-app.use(errorHandler);
+// app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
