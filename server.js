1cd5f877e4cf3cf55221a65b73f78925f5e910e5
Arturo Cullinane <artocully@users.noreply.github.com>
2021-03-31 16:44:01 +0100

    Fix typo

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const errorHandler = require('_middleware/error-handler');
+const errorHandler = require('_middleware/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 // api routes
-// app.use('/users', require('./users/users.controller'));
+app.use('/users', require('./users/user.controller'));
 
 // global error handler
-// app.use(errorHandler);
+app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
