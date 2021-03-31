6a992287e0c10fe4f15663c0205ac335be47e424
Arturo Cullinane <artocully@users.noreply.github.com>
2021-03-31 16:30:51 +0100

    Add initial user service

--- _helpers/db.js
+++ _helpers/db.js
@@ -16,7 +16,7 @@ async function initialize() {
     const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
 
     // init models and add them to the exported db object
-    // db.User = require('../users/user.model')(sequelize);
+    db.User = require('../users/user.model')(sequelize);
 
     // sync all models with database
     await sequelize.sync();
